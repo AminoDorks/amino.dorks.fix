ca649506cdf9259a3d6791cf151cd6c3983c2ad3
misterio060 <83405554+misterio060@users.noreply.github.com>
2025-08-29 19:21:46 +0300

    eshkere

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "requests",
+    "websocket-client==1.3.1", 
+    "setuptools", 
+    "json_minify", 
+    "six",
+    "aiohttp",
+    "websockets"
+]
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name="amino.dorks.fix",
+    license="MIT",
+    author="misterio",
+    version="2.3.6.1",
+    author_email="misterio1234321@gmail.com",
+    description="Library for Amino. Telegram - https://t.me/aminodorks",
+    url="https://github.com/Minori100/Amino.fix",
+    packages=find_packages(),
+    long_description=long_description,
+    install_requires=requirements,
+    keywords=[
+        'aminoapps',
+        'amino.fix',
+        'amino',
+        'amino-bot',
+        'narvii',
+        'api',
+        'python',
+        'python3',
+        'python3.x',
+        'minori'
+    ],
+    python_requires='>=3.6',
+)
