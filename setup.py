e18bb44d3e3d81177ac3f13ab593308cf255eccd
misterio060 <83405554+misterio060@users.noreply.github.com>
2025-08-29 19:53:35 +0300

    version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     name="amino.dorks.fix",
     license="MIT",
     author="misterio",
-    version="1.0.0",
+    version="2.3.6.1",
     author_email="misterio1234321@gmail.com",
     description="Library for Amino. Telegram - https://t.me/aminodorks",
     url="https://github.com/misterio060/amino.dorks.fix",
