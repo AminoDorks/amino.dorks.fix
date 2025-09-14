061594cdb650fc3cd7683b5afe4e53cbb7e563ff
misterio060 <83405554+misterio060@users.noreply.github.com>
2025-09-14 23:34:25 +0300

    fixes

--- aminodorksfix/__init__.py
+++ aminodorksfix/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'amino.dorks.fix'
 __author__ = 'misterio060'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2025 misterio060'
-__version__ = '3.6.6.6'
+__version__ = '3.6.6.7'
 
 from .acm import ACM
 from .client import Client
